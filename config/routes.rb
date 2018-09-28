857c4fe8625dd480985dee4d14a609d2c00da3ee
natsumican0725 <natsumican725.summer@gmail.com>
2018-09-28 14:59:11 +0900

    add-devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+
