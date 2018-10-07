c7d62b44fb98c6f05746fdbd5922dd1213ba25f1
natsumican0725 <natsumican725.summer@gmail.com>
2018-10-07 17:10:08 +0900

    Change-to-uppercase

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/IMG_7191.jpg")
+    image File.open("#{Rails.root}/public/images/IMG_7191.JPG")
     user
     group
   end
