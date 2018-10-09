7e22bc1f4a20fc96d6f8ac61607a53ba12e47696
natsumican0725 <natsumican725.summer@gmail.com>
2018-10-09 12:56:05 +0900

    Change-this-line5

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'natsu-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/natsu-space'
+  config.fog_directory  = 'natsu-space3'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/natsu-space3'
 end
